ae33a1d54ea14831e62d962dceca70a5f74e9497
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-09 12:00:52 -0600

    added render for login and register

--- src/components/Anorak.js
+++ src/components/Anorak.js
@@ -1,4 +1,25 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import './Anorak.css'
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
+export const Anorak = () => (
+    <>
+        <Route render={() => {
+          // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+            if (localStorage.getItem("app_user_id")) {
+                return (
+                    <>
+                        
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+)
