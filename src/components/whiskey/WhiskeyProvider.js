cca126525e08dc928d75f4b2e7ec3eb315ca97b9
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-09 15:07:42 -0600

    defined addWhiskey function and added return statement for WhiskeyContext

--- src/components/whiskey/WhiskeyProvider.js
+++ src/components/whiskey/WhiskeyProvider.js
@@ -1,13 +1,34 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-export const WhiskeyContext = React.createContext()
+export const WhiskeyContext = React.createContext();
 
 export const WhiskeyProvider = (props) => {
-    const [whiskeys, setWhiskeys] = useState([])
+  const [whiskeys, setWhiskeys] = useState([]);
 
-    const getWhiskeys = () => {
-        return fetch ("http://localhost:8089/whiskeys")
-        .then(response => response.json())
-        .then(setWhiskeys)
-    }
-}
+  const getWhiskeys = () => {
+    return fetch("http://localhost:8089/whiskeys")
+      .then((response) => response.json())
+      .then(setWhiskeys);
+  };
+  const addWhiskey = (whiskey) => {
+    return fetch("http://localhost:8089/whiskeys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(whiskey),
+    }).then(getWhiskeys);
+  };
+
+  return (
+    <WhiskeyContext.Provider
+      value={{
+        whiskeys,
+        addWhiskey,
+        getWhiskeys,
+      }}
+    >
+      {props.children}
+    </WhiskeyContext.Provider>
+  );
+};
