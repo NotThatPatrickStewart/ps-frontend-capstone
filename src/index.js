f85e2856829e30508601fd71eaf58fb5a07a5260
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-09 12:10:42 -0600

    changed syntax of Anorak import statememt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Anorak from './components/Anorak';
+import { Anorak } from './components/Anorak';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 
