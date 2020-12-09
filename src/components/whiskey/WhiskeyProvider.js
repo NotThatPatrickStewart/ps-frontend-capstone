205c0f338df655ba0010a55c90f1e2c96e7a3abe
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-09 14:09:10 -0600

    defined WhiskeyProvider function

---
+++ src/components/whiskey/WhiskeyProvider.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react"
+
+export const WhiskeyContext = React.createContext()
+
+export const WhiskeyProvider = (props) => {
+    const [whiskeys, setWhiskeys] = useState([])
+
+    
+}
