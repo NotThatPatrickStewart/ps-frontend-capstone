f52ceecf1c73488578f530ec53dff2b34ededa90
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-09 15:37:14 -0600

    defined ApplicationViews, added return

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -3,3 +3,16 @@ import { Route } from "react-router-dom";
 import { WhiskeyProvider } from "./whiskey/WhiskeyProvider";
 import { WhiskeyList } from "./whiskey/WhiskeyList";
 
+export const ApplicationViews = (props) => {
+  return (
+    <>
+      <WhiskeyProvider>
+        <Route
+          exact
+          path="/search"
+          render={(props) => <WhiskeyList {...props} />}
+        />
+      </WhiskeyProvider>
+    </>
+  );
+};
