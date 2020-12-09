ccddde1e976312e6f3ccdc3701ea7a6d79cef9d7
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-09 14:11:06 -0600

    defined getWhiskeys function

--- src/components/whiskey/WhiskeyProvider.js
+++ src/components/whiskey/WhiskeyProvider.js
@@ -5,5 +5,9 @@ export const WhiskeyContext = React.createContext()
 export const WhiskeyProvider = (props) => {
     const [whiskeys, setWhiskeys] = useState([])
 
-    
+    const getWhiskeys = () => {
+        return fetch ("http://localhost:8089/whiskeys")
+        .then(response => response.json())
+        .then(setWhiskeys)
+    }
 }
