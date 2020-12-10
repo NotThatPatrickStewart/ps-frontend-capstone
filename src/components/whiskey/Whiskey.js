ba10d545f1c1cc34d6ee12b3b3d10cd683216e50
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-10 15:22:04 -0600

    refactored interpolation to attempt to get comparables, incorrect solution

--- src/components/whiskey/Whiskey.js
+++ src/components/whiskey/Whiskey.js
@@ -3,7 +3,8 @@ import "./Whiskey.css"
 
 export const Whiskey = ({ whiskey }) => (
     <section className="whiskey">
-        <h3 className="whiskey__name">WHISKEY: {whiskey.title}</h3>
-        <div className="whiskey__image">{whiskey.img_url}</div>
+        <h3 className="whiskey__name">WHISKEY: {whiskey.comparables.title}</h3>
+        {console.table(whiskey.comparables)}
+        {/* <div className="whiskey__image">{whiskey.img_url}</div> */}
     </section>
 )
