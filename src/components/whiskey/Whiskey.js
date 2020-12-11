37315366caa247787dc6d6e6cdd18420778afbee
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-11 11:08:27 -0600

    changed display text that renders to the dom

--- src/components/whiskey/Whiskey.js
+++ src/components/whiskey/Whiskey.js
@@ -3,14 +3,14 @@ import "./Whiskey.css"
 
 export const Whiskey = ({ whiskey }) => (
     <section className="whiskey">
-        <h3 className="whiskey__name">WHISKEY: {whiskey.title}</h3>
+        <h3 className="whiskey__name">IF YOU LIKE: {whiskey.title}</h3>
         {console.log("whiskey", whiskey)}
         <img className="whiskey__image" src={whiskey.list_img_url} />
         {
                     whiskey.comparables.map(comparable => {
                     return (
                     <>
-                        <h3 className="whiskey__name">COMPARABLES: {comparable.title}</h3>
+                        <h3 className="whiskey__name">YOU MIGHT ALSO LIKE: {comparable.title}</h3>
                         <img className="whiskey__image" src={comparable.list_img_url} />
                     </>
                     )
