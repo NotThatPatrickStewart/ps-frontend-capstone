c933d7640e2ef1a83ad3a86300246f79198f6e3c
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-11 17:26:01 -0600

    changed comparable text to Link

--- src/components/whiskey/Whiskey.js
+++ src/components/whiskey/Whiskey.js
@@ -1,26 +1,31 @@
 import React from "react";
-import "./Whiskey.css"
+import "./Whiskey.css";
+import { Link } from "react-router-dom";
 
 export const Whiskey = ({ whiskey }) => (
-    <section className="whiskey">
-        <h3 className="whiskey__name">IF YOU LIKE: {whiskey.title}</h3>
-        {console.log("whiskey", whiskey)}
-        <img className="whiskey__image" src={whiskey.list_img_url} />
-        {
-                    whiskey.comparables.map(comparable => {
-                    return (
-                    <>
-                        <h3 className="whiskey__name">YOU MIGHT ALSO LIKE: {comparable.title}</h3>
-                        <img className="whiskey__image" src={comparable.list_img_url} />
-                        <button onClick={() => {
+  <section className="whiskey">
+    <h3 className="whiskey__name">IF YOU LIKE: {whiskey.title}</h3>
+    {/* {console.log("whiskey", whiskey)} */}
+    <img className="whiskey__image" src={whiskey.list_img_url} />
+    {whiskey.comparables.map((comparable) => {
+      return (
+        <>
+          <h3 className="whiskey__name">
+            <Link to={`/to-try/${comparable.id}`}>
+              YOU MIGHT ALSO LIKE: {comparable.title}
+            </Link>
+          </h3>
+          <img className="whiskey__image" src={comparable.list_img_url} />
+        </>
+      );
+    })}
+  </section>
+);
+
+{
+  /* <button onClick={() => {
                             comparable.history.push(`/to-try/${comparable.id}`)
                         }} >
-                            TRY
-                        </button>
-                    </>
-                    )
-                    })
-                }
-    </section>
-)
-
+                            from Whiskey.js
+                        </button> */
+}
