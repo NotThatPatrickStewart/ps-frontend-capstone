bea47f1a4e2e96ba74de4603e562fee17684582d
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-11 17:26:26 -0600

    commented out all code on this module for now

--- src/components/whiskey/ToTry.js
+++ src/components/whiskey/ToTry.js
@@ -1,25 +1,33 @@
-import { useContext, useEffect } from "react"
-import { WhiskeyContext } from "./WhiskeyProvider"
+// import { useContext, useEffect, useRef } from "react"
+// import { WhiskeyContext } from "./WhiskeyProvider"
+// import "./Whiskey.css"
 
-export const ToTryButton = (props) => {
-    const {getWhiskey, addWhiskey} = useContext(WhiskeyContext)
+// export const ToTry = (props) => {
+//     const {getWhiskey, addWhiskey} = useContext(WhiskeyContext)
 
-    const title = useRef(null)
+//     const title = useRef(null)
 
-    useEffect(() => {
-        getWhiskey()
-    }, [])
+//     useEffect(() => {
+//         getWhiskey()
+//     }, [])
     
-    const addWhiskeyToTry = () => {
-        const comparableId = parseInt(comprable.current.value)
-        {
-            addWhiskey({
-                title: title.current.value
-            })
-            .then(() => props.history.push("/to-try"))
-        }
-    }
-    return (
-
-    )
-}
+//     const addWhiskeyToTry = () => {
+//         const comparableId = parseInt(comparable.current.value)
+//         {
+//             addWhiskey({
+//                 title: title.current.value
+//             })
+//             .then(() => props.history.push("/to-try"))
+//         }
+//     }
+//     return (
+//         <button type="submit"
+//             onClick={event => {
+//                 event.preventDefault()
+//                 addWhiskeyToTry()
+//             }}
+//             className="btn btn-primary">
+//             from ToTry.js
+//         </button>
+//     )
+// }
