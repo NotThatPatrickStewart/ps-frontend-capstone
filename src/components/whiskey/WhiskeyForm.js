2946cfeeefe63fdd42008602cbba3f5f433ca3de
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-11 11:59:42 -0600

    started defining to check if try button has been clicked

---
+++ src/components/whiskey/WhiskeyForm.js
@@ -0,0 +1,9 @@
+const tryButtonClicked = (event) => {
+    let userId = localStorage.getItem("app_user_id");
+
+        console.log("userId", userId, "conditionId", event.target.id, "checked status", event.target.checked)
+
+        if (event.target.checked) {
+            
+        }
+}
