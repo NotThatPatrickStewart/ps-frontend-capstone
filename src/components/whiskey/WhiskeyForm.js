39b3ef57b6346c1ae913d4ba5b76c40f4a7ad2a9
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-11 14:54:51 -0600

    added and removed code, no saved changes

--- src/components/whiskey/WhiskeyForm.js
+++ src/components/whiskey/WhiskeyForm.js
@@ -1,9 +1,22 @@
-const tryButtonClicked = (event) => {
-    let userId = localStorage.getItem("app_user_id");
 
-        console.log("userId", userId, "conditionId", event.target.id, "checked status", event.target.checked)
 
-        if (event.target.checked) {
-            
-        }
-}
+
+
+
+
+
+
+
+//For adding click to user to add rating to db.json
+// const tryButtonClicked = (event) => {
+//     let userId = localStorage.getItem("app_user_id");
+
+//         console.log("userId", userId, "comparableId", event.target.id, "checked status", event.target.checked)
+
+//         if (event.target.checked) {
+//             addUserComparableId({
+//                 comparableId: parseInt(event.target.id),
+//                 userId: parseInt(userId)
+//             })
+//         } 
+// }
