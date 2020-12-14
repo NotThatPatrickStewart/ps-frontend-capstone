27ed12b9a5384568b6e30707a7f0c3031c88f475
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-14 14:40:55 -0600

    defined HaveTried function to render each whiskey in the Have Tried list to the dom

---
+++ src/components/whiskey/HaveTried.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Whiskey.css";
+
+export const HaveTried = ({ userWhiskey }) => {
+  return <div className="userWhiskeys">
+    <h3 className="whiskey__name">{userWhiskey.title}</h3>
+    <img className="whiskey__image" src={userWhiskey.list_img_url} />
+  </div>;
+  };
