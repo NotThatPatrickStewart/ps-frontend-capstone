5c85f6bb9fe983b6323e9a2b8451d1a3485abdd1
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-14 11:39:08 -0600

    removed Link and To Try button, moved logic to Comparable.js

--- src/components/whiskey/Whiskey.js
+++ src/components/whiskey/Whiskey.js
@@ -1,24 +1,14 @@
 import React from "react";
 import "./Whiskey.css";
-import { Link } from "react-router-dom";
+import { Comparable } from "./Comparable";
 
 export const Whiskey = ({ whiskey }) => (
   <section className="whiskey">
     <h3 className="whiskey__name">IF YOU LIKE: {whiskey.title}</h3>
     {/* {console.log("whiskey", whiskey)} */}
     <img className="whiskey__image" src={whiskey.list_img_url} />
-    {whiskey.comparables.map((comparable) => {
-      return (
-        <>
-          <h3 className="whiskey__name">
-            <Link to={`/to-try/${comparable.id}`}>
-              YOU MIGHT ALSO LIKE: {comparable.title}
-            </Link>
-          </h3>
-          <img className="whiskey__image" src={comparable.list_img_url} />
-        </>
-      );
-    })}
+    {whiskey.comparables.map((comparable) => <Comparable key={comparable.id} comparable={comparable} />
+    )}
   </section>
 );
 
