b5abb49b4e1ab99d55032aa3c50c48e495cba7a3
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-14 12:08:28 -0600

    changed to-try url from dyanmic to static

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { WhiskeyProvider } from "./whiskey/WhiskeyProvider";
-import { WhiskeyList } from "./whiskey/WhiskeySearchList";
+import { WhiskeySearchList } from "./whiskey/WhiskeySearchList";
 import { WhiskeySearch } from "./whiskey/WhiskeySearch";
 import { Comparable } from "./whiskey/Comparable";
 
@@ -15,14 +15,14 @@ export const ApplicationViews = (props) => {
           render={(props) => (
             <>
                 <WhiskeySearch />
-                <WhiskeyList {...props} />
+                <WhiskeySearchList {...props} />
             </>
           )}
 
         />
         <Route
           exact
-          path="/to-try/:comparableId(\d+)"
+          path="/to-try"
           render={(props) => (
             <>
                 <Comparable {...props} />
