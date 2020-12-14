139495e1391bc52316d2ab8d3bc64823173beef1
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-14 14:41:44 -0600

    removed old code that was no longer being used in this module

--- src/components/whiskey/ToTryList.js
+++ src/components/whiskey/ToTryList.js
@@ -25,35 +25,3 @@ export const ToTryList = (props) => {
         </div>
     )
 }
-
-
-
-// export const ToTry = (props) => {
-//     const {getWhiskey, addWhiskey} = useContext(WhiskeyContext)
-
-//     const title = useRef(null)
-
-//     useEffect(() => {
-//         getWhiskey()
-//     }, [])
-    
-//     const addWhiskeyToTry = () => {
-//         const comparableId = parseInt(comparable.current.value)
-//         {
-//             addWhiskey({
-//                 title: title.current.value
-//             })
-//             .then(() => props.history.push("/to-try"))
-//         }
-//     }
-//     return (
-//         <button type="submit"
-//             onClick={event => {
-//                 event.preventDefault()
-//                 addWhiskeyToTry()
-//             }}
-//             className="btn btn-primary">
-//             from ToTry.js
-//         </button>
-//     )
-// }
