c8acf952abe970aaca49c6676d4d7d3e938a108a
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 10:05:47 -0600

    added rating key value pair to HaveTried card

--- src/components/whiskey/HaveTried.js
+++ src/components/whiskey/HaveTried.js
@@ -5,5 +5,6 @@ export const HaveTried = ({ userWhiskey }) => {
   return <div className="userWhiskeys">
     <h3 className="whiskey__name">{userWhiskey.title}</h3>
     <img className="whiskey__image" src={userWhiskey.list_img_url} />
+    <div className="whiskey__rating">RATING: {userWhiskey.rating}</div>
   </div>;
   };
