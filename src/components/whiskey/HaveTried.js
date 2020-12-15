157e3f730eafa9f7f19ef6bac08285f3ed6d6508
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 12:05:27 -0600

    added Link to whiskey name which takes user to the detail page for that specific whiskey id

--- src/components/whiskey/HaveTried.js
+++ src/components/whiskey/HaveTried.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Whiskey.css";
+import { Link } from "react-router-dom"
 
 export const HaveTried = ({ userWhiskey }) => {
-  return <div className="userWhiskeys">
-    <h3 className="whiskey__name">{userWhiskey.title}</h3>
-    <img className="whiskey__image" src={userWhiskey.list_img_url} />
-    <div className="whiskey__rating">RATING: {userWhiskey.rating}</div>
-  </div>;
-  };
+  return (
+    <div className="userWhiskeys">
+      <h3 className="whiskey__name">
+        <Link to={`/userWhiskeys/${userWhiskey.id}`}>{userWhiskey.title}</Link>
+      </h3>
+      <img className="whiskey__image" src={userWhiskey.list_img_url} />
+      <div className="whiskey__rating">RATING: {userWhiskey.rating}</div>
+    </div>
+  );
+};
