5104f549e80873357c0a835c80095a7935cf1fec
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 14:58:49 -0600

    added Add Note button to return statement

---
+++ src/components/whiskey/UserWhiskeyDetail.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from "react";
+import { WhiskeyContext } from "./WhiskeyProvider";
+
+export const UserWhiskeyDetail = (props) => {
+  const { getUserWhiskeyById } = useContext(WhiskeyContext);
+
+  const [userWhiskey, setUserWhiskey] = useState({});
+
+  useEffect(() => {
+    const userWhiskeyId = parseInt(props.match.params.userWhiskeyId);
+    getUserWhiskeyById(userWhiskeyId).then(setUserWhiskey);
+  }, []);
+
+console.log(userWhiskey)
+
+  return (
+    <>
+      <section className="userWhiskeyDetail">
+        <h3 className="userWhiskey__name">{userWhiskey.title}</h3>
+        <img className="userWhiskey__image" src={userWhiskey.list_img_url} />
+        <div className="userWhiskey__region">REGION: {userWhiskey.region}</div>
+        <div className="userWhiskey__region">${userWhiskey.price}</div>
+        <div className="userWhiskey__notes">NOTES: {userWhiskey.notes}</div>
+        {/* <div className="userWhiskey__tags">{userWhiskey.tags.title}</div> */}
+        <button onClick={
+            () => {
+                props.history.push(`/userWhiskeys/edit/${userWhiskey.id}`)
+            }}>
+                ADD NOTES
+            </button>
+      </section>
+    </>
+  );
+};
