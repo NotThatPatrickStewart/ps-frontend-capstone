e4053043b190c20c4036458ee59fef8f8a6ed3d1
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 12:02:03 -0600

    refactored to add '/' before path to fix bug

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,13 +6,13 @@ export const NavBar = (props) => {
     return (
         <ul className="navbar">
             <li className="navbar__item active">
-                <Link className="navbar__link" to="search">SEARCH</Link>
+                <Link className="navbar__link" to="/search">SEARCH</Link>
             </li>
             <li className="navbar__item">
-                <Link className="navbar__link" to="to-try">TO TRY</Link>
+                <Link className="navbar__link" to="/to-try">TO TRY</Link>
             </li>
             <li className="navbar__item">
-                <Link className="navbar__link" to="have-tried">HAVE TRIED</Link>
+                <Link className="navbar__link" to="/have-tried">HAVE TRIED</Link>
             </li>
         </ul>
     )
