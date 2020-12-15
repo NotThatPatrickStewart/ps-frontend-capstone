6ef1a8a844b13a553867de0c4f2f23eaf70b9dfa
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 10:07:36 -0600

    refactored return to only render whiskeys that have a rating which is null

--- src/components/whiskey/ToTryList.js
+++ src/components/whiskey/ToTryList.js
@@ -1,24 +1,25 @@
-import { useContext, useEffect } from "react"
-import { WhiskeyContext } from "./WhiskeyProvider"
-import "./Whiskey.css"
-import { ToTry } from "./ToTry"
+import { useContext, useEffect } from "react";
+import { WhiskeyContext } from "./WhiskeyProvider";
+import "./Whiskey.css";
+import { ToTry } from "./ToTry";
 
 export const ToTryList = (props) => {
-    const { userWhiskeys, getUserWhiskeys } = useContext(WhiskeyContext)
+  const { userWhiskeys, getUserWhiskeys } = useContext(WhiskeyContext);
 
-    useEffect(() => {
-        getUserWhiskeys()
-    }, [])
+  useEffect(() => {
+    getUserWhiskeys();
+  }, []);
 
-        //need if statement that returns on the conditin that userWhiskey.rating === null
-        return (
-        <div className="whiskeys">
-            <>
-            <h1>WHISKEYS TO TRY</h1> 
-            {
-                userWhiskeys.map(userWhiskey => <ToTry key={userWhiskey.id} userWhiskey={userWhiskey} />
-                )}
-            </>
-        </div>
-    )
-}
+  return (
+    <div className="whiskeys">
+      <>
+        <h1>WHISKEYS TO TRY</h1>
+        {userWhiskeys.map((userWhiskey) => {
+          if (userWhiskey.rating === null) {
+            return <ToTry key={userWhiskey.id} userWhiskey={userWhiskey} />;
+          }
+        })}
+      </>
+    </div>
+  );
+};
