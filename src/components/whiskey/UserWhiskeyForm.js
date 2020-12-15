536946126bc857776d3fbbd3a3aace1ec634c221
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 16:11:20 -0600

    refactored UserWhiskeyForm function- form is rendering but data is not yet being saved correctly, WIP

--- src/components/whiskey/UserWhiskeyForm.js
+++ src/components/whiskey/UserWhiskeyForm.js
@@ -1,22 +1,43 @@
+import React, { useContext, useRef } from "react";
+import { WhiskeyContext } from "./WhiskeyProvider";
+import "./Whiskey.css";
 
+export const UserWhiskeyForm = (props) => {
+    const { userWhiskeys, updateUserWhiskey, } = useContext(WhiskeyContext)
 
+    const title = useRef(null)
 
+   const addWhiskeyNote = () => {
+        
+        if ((userWhiskey.rating === null)) {
 
-
-
-
-
-
-//For adding click to user to add rating to db.json
-const tryButtonClicked = (event) => {
-    let userId = localStorage.getItem("app_user_id");
-
-        console.log("userId", userId, "comparableId", event.target.id, "checked status", event.target.checked)
-
-        if (event.target.checked) {
-            addUserComparableId({
-                comparableId: parseInt(event.target.id),
-                userId: parseInt(userId)
-            })
-        } 
-}
+       updateUserWhiskey({
+            title: title.current.value
+       })
+       .then(() => props.history.push("/to-try"))
+    } else {
+        updateUserWhiskey({
+            title: title.current.value
+       })
+       .then(() => props.history.push("/have-tried"))
+    }
+   }
+   return (
+       <form className="addNote">
+           <fieldset>
+               <div className="form-note">
+                   <label htmlFor="whiskeyNote">ADD NOTE</label>
+                   <input type="text" id="whiskeyNote" ref={title} required autoFocus className="form-control" placeholder="whiskey notes" />
+               </div>
+           </fieldset>
+           <button type="submit"
+                onClick={evt => {
+                    evt.preventDefault() // Prevent browser from submitting the form
+                    addWhiskeyNote()
+                }}
+                className="btn btn-primary">
+                SAVE NOTE
+            </button>
+       </form>
+   )
+ }
