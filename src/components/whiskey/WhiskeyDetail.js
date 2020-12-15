e304bcbafbb2c61a45de426929bd12bd4e6c5ec1
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-15 12:26:27 -0600

    defined UserWhiskeyDetail function

---
+++ src/components/whiskey/WhiskeyDetail.js
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect, useState } from "react";
+import { WhiskeyContext } from "./WhiskeyProvider";
+
+export const UserWhiskeyDetail = (props) => {
+  const { getUserWhiskeyById } = useContext(WhiskeyContext);
+
+  const [userWhiskey, setUserWhiskey] = useState({});
+
+  useEffect(() => {
+    const userWhiskeyId = parseInt(props.match.params.userWhiskeyId);
+    getUserWhiskeyById(userWhiskeyId).then(setUserWhiskey);
+  }, []);
+
+  return (
+    <>
+      <section className="userWhiskeyDetail">
+        <h3 className="userWhiskey__name">{userWhiskey.name}</h3>
+        <img className="userWhiskey__image" src={userWhiskey.list_img_url} />
+        <div className="userWhiskey__region">REGION: {userWhiskey.region}</div>
+        <div className="userWhiskey__notes">${userWhiskey.notes}</div>
+        {/* <div className="userWhiskey__tags">{userWhiskey.tags.title}</div> */}
+      </section>
+    </>
+  );
+};
