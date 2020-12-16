c433e62271e421c282fcf32534e55171f57429fa
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-16 17:31:17 -0600

    defined ToTryUserWhiskeyForm function, which renders the edit fields for a given whiskey and allows you to add notes, and add a arting which moves the whiskey to the HaveTriedList

---
+++ src/components/whiskey/ToTryUserWhiskeyForm.js
@@ -0,0 +1,64 @@
+import React, { useContext, useRef } from "react";
+import { WhiskeyContext } from "./WhiskeyProvider";
+import "./Whiskey.css";
+import { useHistory } from "react-router-dom";
+
+export const ToTryUserWhiskeyForm = (props) => {
+  const { updateUserWhiskey } = useContext(WhiskeyContext);
+
+  const notes = useRef(null);
+  const rating = useRef(null);
+
+  let history = useHistory()
+
+  const addWhiskeyNote = () => {
+      const userWhiskeyId = parseInt(props.match.params.userWhiskeyId)
+    updateUserWhiskey(userWhiskeyId, {
+      notes: notes.current.value,
+      rating: parseInt(rating.current.value),
+    }).then(() => props.history.push("/to-try"));
+  };
+  return (
+    <form className="addNote">
+      <fieldset>
+        <div className="form-notes">
+          <label htmlFor="whiskeyNote">ADD NOTE</label>
+          <input
+            type="text"
+            id="whiskeyNote"
+            ref={notes}
+            required
+            autoFocus
+            className="form-control"
+            placeholder="whiskey notes"
+          />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className="form-rating">
+          <label htmlFor="whiskeyRating">ADD RATING 1-100</label>
+          <input
+            type="text"
+            id="whiskeyRating"
+            ref={rating}
+            required
+            autoFocus
+            className="form-control"
+            placeholder="whiskey rating"
+          />
+        </div>
+      </fieldset>
+      <button
+        type="submit"
+        onClick={(evt) => {
+          evt.preventDefault(); // Prevent browser from submitting the form
+          addWhiskeyNote();
+        }}
+        className="btn btn-primary"
+      >
+        SAVE NOTE
+      </button>
+      <button onClick={() => history.goBack()}>Back</button>
+    </form>
+  );
+};
