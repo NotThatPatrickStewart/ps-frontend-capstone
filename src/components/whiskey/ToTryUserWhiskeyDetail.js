69d4c35335723d3caab487001d9048fd9ccb6da4
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-17 14:16:31 -0600

    refactored useEffect to check where data was coming from, data is now being pulled from correct API (region and price from 'externa' API)

--- src/components/whiskey/ToTryUserWhiskeyDetail.js
+++ src/components/whiskey/ToTryUserWhiskeyDetail.js
@@ -3,9 +3,10 @@ import { WhiskeyContext } from "./WhiskeyProvider";
 import { useHistory } from "react-router-dom";
 
 export const ToTryUserWhiskeyDetail = (props) => {
-  const { getUserWhiskeyById, deleteWhiskey } = useContext(WhiskeyContext);
+  const { getUserWhiskeyById, deleteWhiskey, getWhiskeyById } = useContext(WhiskeyContext);
 
   const [userWhiskey, setUserWhiskey] = useState({});
+  const [whiskey, setWhiskey] = useState({});
 
   let history = useHistory()
 
@@ -14,7 +15,20 @@ export const ToTryUserWhiskeyDetail = (props) => {
     getUserWhiskeyById(userWhiskeyId).then(setUserWhiskey);
   }, []);
 
-console.log(userWhiskey)
+  useEffect(() => {
+    if (userWhiskey.id !== undefined) { //added conditional, this removed the first fetch call error 404 not found where id returned as NaN
+    const whiskeyId = parseInt(userWhiskey.whiskeyId);
+    getWhiskeyById(whiskeyId)
+    .then(w => {
+     console.log("w", w) 
+     setWhiskey(w)
+    }
+    )};
+    // console.log("whiskeyId", whiskeyId) //this concolse log breaks everything
+  }, []); //removed userWhiskey from array base state, this removed the second fetch call error 404 not found where id returned as an integer
+
+console.log("userWhiskey", userWhiskey)
+ console.log("whiskey", whiskey) //this console log returns an empty object
 
   return (
     <>
