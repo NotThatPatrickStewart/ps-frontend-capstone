b9ba265f6a1ba415ce744f41b88eed794b68dcbb
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2021-03-25 19:55:24 -0600

    added link for logout

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,17 +3,34 @@ import { Link } from "react-router-dom"
 import "./NavBar.css"
 
 export const NavBar = (props) => {
+    const clearLocalStorage = () => {
+        localStorage.removeItem("app_user")
+    }
+
+    const currentUser = localStorage.getItem("app_user")
+    
     return (
-        <ul className="navbar">
-            <li className="navbar__item active">
+        <div className="navbar">
+            <h1 className="navbar__title">
+                ANORAK
+            </h1>
+            <p className="navbar__subtitle">
+                FOR FANS OF THE UNUSUAL
+            </p>
+            <div className="navbar__links">
+            <p className="navbar__item active">
                 <Link className="navbar__link" to="/">SEARCH</Link>
-            </li>
-            <li className="navbar__item">
+            </p>
+            <p className="navbar__item">
                 <Link className="navbar__link" to="/to-try">TO TRY</Link>
-            </li>
-            <li className="navbar__item">
+            </p>
+            <p className="navbar__item">
                 <Link className="navbar__link" to="/have-tried">HAVE TRIED</Link>
-            </li>
-        </ul>
+            </p>
+            <p className="navbar__item logout">
+                    {localStorage.getItem("app_user") ? <Link className="navbar__link" to="/login" onClick={()=>{clearLocalStorage()}}>logout</Link>:<Link className="navbar__link" to="/login">login</Link> }
+            </p>
+            </div>
+        </div>
     )
 }
